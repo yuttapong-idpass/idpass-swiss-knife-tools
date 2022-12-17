085397e35bcfb7a1f932a45b7e0cbe7611b5720d
yuttapong pimpralup <yuttapong@idpassglobal.com>
2022-12-18 03:19:49 +0700

    🪄feat: change icon

--- src/components/OperatorPage/OperatorPage.tsx
+++ src/components/OperatorPage/OperatorPage.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import FloppyDisk from "../../assets/images/floppy-disk.png";
-import Maximize from "../../assets/images/maximize.png";
+import FullScreen from "../../assets/images/full-screen.png";
 
-import './OperatorPage.css';
+import "./OperatorPage.css";
 
 type Props = {};
 
@@ -11,24 +11,46 @@ const OperatorPage = (props: Props) => {
     <div>
       <div className="flex flex-col">
         <div className="h-screen">
-          <div className=" bg-gray-900" style={{ height: "5%" }}>
+          <div className=" bg-gray-900" style={{ height: "6%" }}>
             <nav className="flex items-center justify-between flex-wrap bg-gray-900 p-1">
               <div className="flex items-center flex-shrink-0 text-white mr-6">
-                <img src={FloppyDisk}  className="fill-current h-8 w-8 mr-2 p-1" />
-                <img src={Maximize}  className="fill-current h-8 w-8 mr-2 p-1" />
+                <img
+                  src={FloppyDisk}
+                  className="fill-current h-8 w-8 mr-2 p-1"
+                />
+                <img
+                  src={FullScreen}
+                  className="fill-current h-8 w-8 mr-2 p-1"
+                />
               </div>
             </nav>
           </div>
           <div className="" style={{ height: "40%" }}>
-            <textarea className="w-full h-full resize-none p-2" placeholder="Input here ..."></textarea>
+            <textarea
+              className="w-full h-full resize-none p-2"
+              placeholder="Input here ..."
+            ></textarea>
           </div>
 
-          <div className="border" style={{ height: "9%" }}>
+          <div className="border" style={{ height: "7%" }}>
             Action Button
           </div>
 
-          <div className="border bg-gray-900" style={{ height: "5%" }}>
-            Navbar
+          <div className="border bg-gray-900" style={{ height: "6%" }}>
+            <div className=" bg-gray-900" style={{ height: "6%" }}>
+              <nav className="flex items-center justify-between flex-wrap bg-gray-900 p-1">
+                <div className="flex items-center flex-shrink-0 text-white mr-6">
+                  <img
+                    src={FloppyDisk}
+                    className="fill-current h-8 w-8 mr-2 p-1"
+                  />
+                  <img
+                    src={FullScreen}
+                    className="fill-current h-8 w-8 mr-2 p-1"
+                  />
+                </div>
+              </nav>
+            </div>
           </div>
           <div className="border" style={{ height: "40%" }}>
             Output content
