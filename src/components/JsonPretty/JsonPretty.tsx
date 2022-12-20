c96d0208f34de7637de6f349a5d7cee6fdb32a97
Yuttapong Pimpralup[IDPG] <yuttapong@idpassglobal.com>
2022-12-20 17:52:24 +0700

    🪄feat: add color

--- src/components/JsonPretty/JsonPretty.tsx
+++ src/components/JsonPretty/JsonPretty.tsx
@@ -19,10 +19,6 @@ type Props = {
   isTreeView: boolean;
 };
 
-const json = { hello: "world", Test: ["hello"] };
-
-const test = JSON.stringify(json, undefined, 4);
-
 const theme = {
   scheme: "monokai",
   author: "wimer hazenberg (http://www.monokai.nl)",
@@ -52,12 +48,11 @@ const JsonPretty = (props: Props) => {
   const [jsonArea, setJsonArea] = useState({});
   const [isTreeView, setTreeView] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [messageError, setMessageError] = useState('');
+  const [messageError, setMessageError] = useState("");
 
   const handleValue = (event$: any) => {
     try {
       let setting = JSON.parse(event$.target.value);
-      console.log("settings -->", setting);
       setJsonArea(setting);
       setIsError(false);
     } catch (error: any) {
@@ -65,35 +60,62 @@ const JsonPretty = (props: Props) => {
       setMessageError(String(error));
       setJsonArea({});
     }
-
-    // setJsonArea(JSON.parse(event$.target.value));
   };
 
   const handleClick = () => {
     const data: any = jsonArea;
     if (isError) {
-      dispatch(inputJson({ item: {}, isError: true, messageError: messageError }));
-    } else { 
-      dispatch(inputJson({ item: data, isError: false, messageError: '' }));
+      dispatch(
+        inputJson({ item: {}, isError: true, messageError: messageError })
+      );
+    } else {
+      dispatch(inputJson({ item: data, isError: false, messageError: "" }));
     }
-    
   };
 
   return (
     <div>
       <div className="flex flex-col">
         <div className="h-screen">
-          <div className=" bg-gray-900" style={{ height: "6%" }}>
-            <nav className="flex flex-wrap bg-gray-900 p-1">
-              <div className="flex flex-shrink-0 text-white mr-6 ">
+          <div className="" style={{ height: "6%" }}>
+            <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-1">
+              <div className="flex items-center flex-shrink-0 text-white mr-6">
+                {/* <svg
+                  className="fill-current h-8 w-8 mr-2"
+                  width="54"
+                  height="54"
+                  viewBox="0 0 54 54"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+                </svg>
+                <span className="font-semibold text-xl tracking-tight">
+                  Tailwind CSS
+                </span> */}
                 <img
                   src={FloppyDisk}
-                  className="fill-current h-8 w-8 mr-2 p-1"
+                  className="fill-current h-6 w-6 mr-2  cursor"
+                  width={"50%"}
+                  height={"50%"}
                 />
                 <img
                   src={FullScreen}
-                  className="fill-current h-8 w-8 mr-2 p-1"
+                  className="fill-current h-6 w-6 mr-2  cursor"
+                  width={"50%"}
+                  height={"50%"}
                 />
+              </div>
+              <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div className="text-sm lg:flex-grow"></div>
+                <div>
+                  <input type="text" className="inline-block text-sm px-4 py-2 leading-none border rounded text-teal-500 border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 mr-6" />
+                  <a
+                    href="#"
+                    className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+                  >
+                    Download
+                  </a>
+                </div>
               </div>
             </nav>
           </div>
@@ -141,11 +163,13 @@ const JsonPretty = (props: Props) => {
             {/* { props.isJsonPretty ? <JsonPretty isTreeView={false} /> : null} */}
 
             {jsonPrettyReducer.isError ? (
-              <pre style={{color: 'red'}}>{ jsonPrettyReducer.messageError }</pre>
+              <pre style={{ color: "red" }}>
+                {jsonPrettyReducer.messageError}
+              </pre>
             ) : (
               <div>
                 {isTreeView ? (
-                  <JSONTree data={ jsonPrettyReducer.item } theme={theme} />
+                  <JSONTree data={jsonPrettyReducer.item} theme={theme} />
                 ) : (
                   <pre id="jsonArea">
                     {JSON.stringify(jsonPrettyReducer.item, null, 2)}
