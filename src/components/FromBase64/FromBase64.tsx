37a7d3f75ac32743a807d2a20351051f8c9b2365
yuttapong pimpralup <yuttapong@idpassglobal.com>
2022-12-29 00:41:12 +0700

    🪄feat: add image

--- src/components/FromBase64/FromBase64.tsx
+++ src/components/FromBase64/FromBase64.tsx
@@ -3,21 +3,32 @@ import FloppyDiskImage from "../../assets/images/floppy-disk.png";
 import FullScreenImage from "../../assets/images/full-screen.png";
 import ExitFullScreenImage from "../../assets/images/exit-fullscreen.png";
 import CopyToClipboardImage from "../../assets/images/copy-to-clipboard.png";
-import Upload from '../../assets/images/photo.png';
+import Upload from "../../assets/images/photo.png";
 import "./FromBase64.css";
 
 type Props = {};
 
 const FromBase64 = (props: Props) => {
   const options = [
-    { value: "image", text: "to image" },
     { value: "string", text: "to string" },
+    { value: "image", text: "to image" },
   ];
 
   const [selected, setSelected] = useState(options[0].value);
+  const [base64, setBase64] = useState("");
 
   const handleSelectOption = ($event: any) => {
     setSelected($event.target.value);
+  };
+
+  const imageUpload = (event$: any) => {
+    const fileUploaded = event$.target.files[0];
+    const reader: any = new FileReader();
+    reader.onload = () => {
+      let base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+      setBase64(base64String);
+    };
+    reader.readAsDataURL(fileUploaded);
   };
 
   return (
@@ -40,11 +51,26 @@ const FromBase64 = (props: Props) => {
                       className="fill-current h-8 w-8 mr-2 p-1 cursor"
                       onClick={() => {}}
                     />
-                    <img 
+                    {/* <img 
                         src={Upload}
                         className="fill-current h-8 w-8 mr-2 p-1 cursor"
-                        onClick={() => {}}
-                    />
+                        onClick={imageUpload}
+                      
+                    /> */}
+                    <div className="image-upload">
+                      <label htmlFor="file-input">
+                        <img
+                          src={Upload}
+                          className="fill-current h-8 w-8 mr-2 p-1 cursor"
+                        />
+                      </label>
+                      <input
+                        id="file-input"
+                        type="file"
+                        onChange={imageUpload}
+                      />
+                    </div>
+
                     <select
                       className="inline-block text-sm px-3 py-1 leading-none border rounded text-black border-white  hover:bg-white mt-4 lg:mt-0"
                       value={selected}
@@ -60,11 +86,11 @@ const FromBase64 = (props: Props) => {
                 </nav>
               </div>
               <div className="border grow">
-                <textarea
+                {/* <textarea
                   id="inputText"
                   className="w-full h-full resize-none p-2"
                   placeholder="Input here ..."
-                ></textarea>
+                ></textarea> */}
               </div>
             </div>
           </div>
@@ -92,8 +118,13 @@ const FromBase64 = (props: Props) => {
                   </div>
                 </nav>
               </div>
-              <div className="border grow text-output">
-                <div className="p-3"></div>
+              <div className="border grow">
+                <div className="h-full">
+                  <textarea
+                    className="w-full h-full resize-none p-4"
+                    value={base64}
+                  ></textarea>
+                </div>
               </div>
             </div>
           </div>
