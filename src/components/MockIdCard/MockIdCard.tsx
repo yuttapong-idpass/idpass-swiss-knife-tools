5dc35028018dd4eac1796297c4c904f2147d201f
yuttapong pimpralup <yuttapong@idpassglobal.com>
2023-02-07 00:43:03 +0700

    fea; add card mock

--- src/components/MockIdCard/MockIdCard.tsx
+++ src/components/MockIdCard/MockIdCard.tsx
@@ -444,8 +444,32 @@ const MockIdCard = (props: Props) => {
                     "
               >
                 <h1 className="text-xl font-bold text-gray-900">
-                  หมายเลขบัตรประชาชน : ss
+                  บัตรประชาชน :
                 </h1>
+
+                <br />
+
+                <ul>
+                  <div className="container">
+                    <img
+                      src={ThaiIdCardTemplate}
+                      alt="Snow"
+                      style={{ width: "100%" }}
+                    />
+                    <div className="bottom-left id-card-font numbers">
+                      1234567
+                    </div>
+                    <div className="top-left id-card-font numbers">
+                      1 1008 00832 15 6
+                    </div>
+                    <div className="top-left-name id-card-font numbers">
+                      นาย สมทวย คงควรคอย
+                    </div>
+                    <div className="top-right">Top Right</div>
+                    <div className="bottom-right">Bottom Right</div>
+                    <div className="centered">Centered</div>
+                  </div>
+                </ul>
               </div>
 
               <div>
