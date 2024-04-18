31cacdb3b9b6b83e9d6017cdb41da8d5e9f65537
yuttapong idpass <yuttapong@idpassglobal.com>
2024-04-19 00:04:31 +0700

    set example barcode

--- src/container/BarCodeGenerator/BarCodeGenerator.tsx
+++ src/container/BarCodeGenerator/BarCodeGenerator.tsx
@@ -5,39 +5,56 @@ type Props = {};
 
 const BarCodeGenerator = (props: Props) => {
   const barcodeTypes = [
-    { name: "code128", value: "code128" },
-    { name: "ean13", value: "ean13" },
-    { name: "ean8", value: "ean8" },
-    { name: "ean5", value: "ean5" },
-    { name: "ean2", value: "ean2" },
-    { name: "upc", value: "upc" },
-    { name: "code39", value: "code39" },
-    { name: "itf14", value: "itf14" },
-    { name: "msi", value: "msi" },
-    { name: "pharmacode", value: "pharmacode" },
+    { name: "code128", value: "code128", example: "Hi" },
+    { name: "ean13", value: "ean13", example: "1234567890128" },
+    { name: "ean8", value: "ean8", example: "12345670" },
+    { name: "ean5", value: "ean5", example: "12345" },
+    { name: "ean2", value: "ean2", example: "12" },
+    { name: "upc-a", value: "upc", example: "123456789012" },
+    { name: "code39", value: "code39", example: "Hello" },
+    { name: "itf14", value: "itf14", example: "1234567890123" },
+    { name: "msi", value: "msi", example: "123456" },
+    { name: "pharmacode", value: "pharmacode", example: "12345" },
   ];
 
   const [barcodeType, setBarCodeType] = useState(barcodeTypes[0].value);
-  const [barcodeInput, setBarCodeInput] = useState("");
+  const [example, setExample] = useState("ABC-1234");
+  const [barcodeInput, setBarCodeInput] = useState("ABC-1234");
 
-  const handleSelectBarCodeType = (event$: SyntheticEvent<EventTarget>) => { 
+  const handleSelectBarCodeType = (event$: SyntheticEvent<EventTarget>) => {
     const type = (event$.target as HTMLInputElement).value;
+    const indexExample: number = barcodeTypes.findIndex(
+      (item) => item.value === type
+    );
+
+    const getValueExample = barcodeTypes[indexExample].example;
+
+    setExample(getValueExample);
+    setBarCodeInput(getValueExample);
     setBarCodeType(type);
-  }
+  };
 
-  const handleBarCodeInput = (event$: SyntheticEvent<EventTarget>) => { 
+  const handleBarCodeInput = (event$: SyntheticEvent<EventTarget>) => {
+    const value = (event$.target as HTMLInputElement).value;
+    setBarCodeInput(value);
+  };
 
-  }
+  const onClickGenerateBarCode = () => {
+    renderBarCode();
+  };
 
-  useEffect(() => {
-    JsBarcode("#barcode", "HI!", {
-      fontSize: 40,
+  const renderBarCode = () => {
+    JsBarcode("#barcode", barcodeInput, {
+      fontSize: 20,
       background: "#ffffff",
       lineColor: "#000000",
-      text: "HI!",
-      format: "",
+      format: barcodeType,
     });
-  }, []);
+  };
+
+  useEffect(() => {
+    renderBarCode();
+  }, [example]);
 
   return (
     <div className="w-full bg-primary p-4 flex flex-col gap-2">
@@ -60,6 +77,7 @@ const BarCodeGenerator = (props: Props) => {
                 name="type"
                 id="type"
                 className="rounded-md text-primary bg-primary p-2"
+                onChange={handleSelectBarCodeType}
               >
                 {barcodeTypes.map((item, index) => (
                   <option key={index} value={item.value}>
@@ -71,7 +89,9 @@ const BarCodeGenerator = (props: Props) => {
             <div>
               <input
                 type="text"
+                value={barcodeInput}
                 className="rounded-md text-primary bg-primary p-2 shadow-lg"
+                onChange={handleBarCodeInput}
               />
             </div>
             <div>
@@ -97,6 +117,7 @@ const BarCodeGenerator = (props: Props) => {
                     hover:bg-lime-400
                     dark:bg-lime-300
                     dark:hover:bg-lime-500"
+                onClick={onClickGenerateBarCode}
               >
                 Generate
               </button>
@@ -128,6 +149,11 @@ const BarCodeGenerator = (props: Props) => {
                 Refresh
               </button>
             </div>
+          </div>
+          <div>
+            <span className="text-primary text-lg">
+              Example Format: {example}{" "}
+            </span>
           </div>
           <div className="flex flex-cols">
             <div>
