c42b15e8fa1c28acd41c5936ce7a8a4683c38ad9
yuttapong idpass <yuttapong@idpassglobal.com>
2025-11-15 19:57:14 +0700

    upgrade tailwind v4 to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000},
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/'
 })
