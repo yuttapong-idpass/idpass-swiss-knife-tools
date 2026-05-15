3f88c5ca7cabbd80f9b0dc7484a6e78c33901b40
yuttapong idpass <yuttapong@idpassglobal.com>
2026-05-15 18:13:21 +0700

    feat: integrate json_typegen_wasm for JSON to TypeScript conversion
    - Added `json_typegen_wasm` dependency to facilitate JSON to TypeScript interface conversion.
    - Updated `JsonToInterface` component to utilize the `run` function from `json_typegen_wasm` for improved conversion logic.
    - Refactored the conversion logic to simplify the process and enhance error handling.
    - Updated Vite configuration to include new plugins: `vite-plugin-top-level-await` and `vite-plugin-wasm` for better support of async operations and WebAssembly.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import path from "path";
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [react(), tailwindcss(),],
+  plugins: [wasm(), topLevelAwait(), react(), tailwindcss()],
   base: '/',
   resolve: {
     alias: {
